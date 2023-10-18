42926d051ed8c51cf24aa2eb8e75b200d971f408
dharamraj-dev <dharamrajyadav6312@gmail.com>
2023-10-18 17:34:12 +0530

    News Components Add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import NavBar from './Components/NavBar';
+import { News } from './Components/News';
 
 import React, { Component } from 'react'
 
 export default class App extends Component {
-  c = "Dharam"
   render() {
     return (
-      <div>This is my first class based component {this.c}</div>
+      <>
+        <NavBar/>
+        <News/>
+      </>
     )
   }
 }
---
+++ src/Components/NewsItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component{
+    
+    render(){
+        let {title, description, imageUrl, newsUrl} = this.props;
+        return(
+            <div className="my-3">
+                <div className="card">
+                    <img src={imageUrl} className="card-img-top" alt="img"/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href="/newsdetails" className="btn btn-sm btn-primary">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
