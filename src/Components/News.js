69aa5ac5bafefbca389991e3815571e1607394d9
dharamraj-dev <dharamrajyadav6312@gmail.com>
2023-10-20 12:02:02 +0530

    Adding New Category in News App

--- src/Components/News.js
+++ src/Components/News.js
@@ -7,23 +7,50 @@ export class News extends Component{
         console.log("Hello iam a constructor from news components")
         this.state={
             articles: [],
-            loading: false
+            loading: false,
+            page: 1
         }
     }
 
     async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e1044516e091486892a3b049236c1a4c"
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=e1044516e091486892a3b049236c1a4c&page=1"
         let data = await fetch(url)
         let parsedata = await data.json()
         console.log(parsedata);
         this.setState({articles: parsedata.articles})
     }
+
+    handlePrevClick = async()=>{
+        console.log("Previews")
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e1044516e091486892a3b049236c1a4c&page=${this.state.page - 1}`
+        let data = await fetch(url)
+        let parsedata = await data.json()
+        console.log(parsedata);
+        this.setState({articles: parsedata.articles})
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedata.articles
+        })
+    }
+    handleNextClick = async()=>{
+        console.log("Next")
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=e1044516e091486892a3b049236c1a4c&page=${this.state.page + 1}`
+        let data = await fetch(url)
+        let parsedata = await data.json()
+        console.log(parsedata);
+        this.setState({articles: parsedata.articles})
+        this.setState({
+            page: this.state.page + 1,
+            articles: parsedata.articles
+        })
+    }
+
     render(){
         console.log("Render")
         return(
             <>
-            <div className="container">
-                <h2>News Daily - Top Heading</h2>
+            <div className="container my-5">
+            <h2><marquee behavior="scroll" direction="left">News Daily - Cricket, Business, Technology, Stock Market, Money, Tranding Topics etc.</marquee></h2>
                 <div className="row">
                 {this.state.articles.map((element)=>{
                    return <div className="col-md-4 col-12" key={element.url}>
@@ -31,6 +58,10 @@ export class News extends Component{
                </div>
                 })}
                 </div>
+            </div>
+            <div className="container d-flex justify-content-between">
+                <button disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previews</button>
+                <button className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
             </div>
             </>
         )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component{
         return(
             <div className="my-3">
                 <div className="card">
-                    <img src={!imageUrl?"https://www.hindustantimes.com/ht-img/img/2023/10/17/1600x900/Screenshot_2023-10-18_011528_1697571947440_1697571956584.png":imageUrl} className="card-img-top" alt="img"/>
+                    <img src={!imageUrl?"https://www.hindustantimes.com/ht-img/img/2023/10/17/1600x900/Screenshot_2023-10-18_011528_1697571947440_1697571956584.png":imageUrl} style={{ height: "200px" }} className="card-img-top" alt="img"/>
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                        <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
